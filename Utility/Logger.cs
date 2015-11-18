6a9a809f0ae9b7502d5a9d3f22d986fdc7666588
Andrew <a.frailing@gmail.com>
2015-11-18 09:39:56 -0500

    Updated the logger

--- Utility/Logger.cs
+++ Utility/Logger.cs
@@ -3,6 +3,7 @@
 namespace Utility {
     public static class Logger {
         static ApplicationErrorLog _errorLog;
+        static object _lockObject = new object();
 
         static string _applicationLoggerPath;
         public static string ApplicationLoggerPath {
@@ -13,6 +14,14 @@ namespace Utility {
             }
         }
 
+        public static ConsoleColor ColorForExceptions { get; set; } = ConsoleColor.Red;
+        public static ConsoleColor ColorForErrors { get; set; } = ConsoleColor.Red;
+        public static ConsoleColor ColorForInformation { get; set; } = ConsoleColor.White;
+        public static ConsoleColor ColorForDebug { get; set; } = ConsoleColor.DarkGreen;
+        public static ConsoleColor ColorForTimeStamp { get; set; } = ConsoleColor.White;
+        public static ConsoleColor ColorForAppName { get; set; } = ConsoleColor.White;
+        public static ConsoleColor ColorForSperator { get; set; } = ConsoleColor.White;
+        public static string TimeStampFormat { get; set; } = "";
         public static string ApplicationDisplayName { get; set; } = "";
 
         public static void RegisterContext(string fieldName, string information) {
@@ -21,33 +30,42 @@ namespace Utility {
 
         public static void LogException(Exception ex) {
             if(_errorLog != null) _errorLog.LogException(ex);
-            ColoredConsole.WriteLine(ConsoleColor.Red, $"{ApplicationDisplayName}: Exception: {ex.GetType()} -- {ex.Message}");
+            WriteAtomic(() => ColoredConsole.WriteLine(ColorForExceptions, $"Exception: {ex.GetType()} -- {ex.Message}"));
         }
 
         public static void LogError(string msg) {
             if(_errorLog != null) _errorLog.LogMessage(msg);
-            ColoredConsole.WriteLine(ConsoleColor.Red, $"{ApplicationDisplayName}: {msg}");
+            WriteAtomic(() => ColoredConsole.WriteLine(ColorForErrors, msg));
         }
 
         public static void LogError(string formatString, params object[] args) {
             if(_errorLog != null) _errorLog.LogMessage(string.Format(formatString, args));
-            ColoredConsole.WriteLine(ConsoleColor.Red, $"{ApplicationDisplayName}: {formatString}", args);
+            WriteAtomic(() => ColoredConsole.WriteLine(ColorForErrors, formatString, args));
         }
 
         public static void LogInformation(string msg) {
-            Console.WriteLine($"{ApplicationDisplayName}: {msg}");
+            WriteAtomic(() => ColoredConsole.WriteLine(ColorForInformation, msg));
         }
 
         public static void LogInformation(string formatString, params object[] args) {
-            Console.WriteLine($"{ApplicationDisplayName}: {formatString}", args);
+            WriteAtomic(() => ColoredConsole.WriteLine(ColorForInformation, formatString, args));
         }
 
         public static void LogDebug(string msg) {
-            ColoredConsole.WriteLine(ConsoleColor.Green, $"{ApplicationDisplayName}: {msg}");
+            WriteAtomic(() => ColoredConsole.WriteLine(ColorForDebug, msg));
         }
 
         public static void LogDebug(string formatString, params object[] args) {
-            ColoredConsole.WriteLine(ConsoleColor.Green, $"{ApplicationDisplayName}: {formatString}", args);
+            WriteAtomic(() => ColoredConsole.WriteLine(ColorForDebug, formatString, args));
+        }
+
+        private static void WriteAtomic(Action writeMessage) {
+            lock (_lockObject) {
+                ColoredConsole.Write(ColorForTimeStamp, $"[{DateTime.Now.ToString(TimeStampFormat)}]");
+                if(!string.IsNullOrEmpty(ApplicationDisplayName)) ColoredConsole.Write(ColorForAppName, $" {ApplicationDisplayName}");
+                ColoredConsole.Write(ColorForSperator, ": ");
+                writeMessage();
+            }
         }
     }
 }
