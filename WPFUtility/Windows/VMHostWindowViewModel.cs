2f22d634e1e547cc1c7ac653479b73c953ab6154
Andrew <a.frailing@gmail.com>
2016-02-05 12:52:13 -0500

    Added a background color for the windows

--- WPFUtility/Windows/VMHostWindowViewModel.cs
+++ WPFUtility/Windows/VMHostWindowViewModel.cs
@@ -2,10 +2,12 @@
 
 namespace WPFUtility {
     public class VMHostWindowViewModel : ViewModelBase {
-        public VMHostWindowViewModel(ViewModelBase content) {
+        public VMHostWindowViewModel(ViewModelBase content, string backgroundColor) {
             Content = content;
+            BackgroundColor = backgroundColor;
         }
         
         public ViewModelBase Content { get; }
+        public string BackgroundColor { get; }
     }
 }
--- WPFUtility/Windows/WindowGenerator.cs
+++ WPFUtility/Windows/WindowGenerator.cs
@@ -3,22 +3,22 @@ using System.Windows;
 
 namespace WPFUtility {
     public static class WindowGenerator {
-        public static void CreateAndShow(Func<Action, ViewModelBase> contentGenerator, bool resizeable, double height, double width) {
+        public static void CreateAndShow(Func<Action, ViewModelBase> contentGenerator, bool resizeable, double height, double width, string backgroundColor) {
             var window = new VMHostWindowView();
-            var content = new VMHostWindowViewModel(contentGenerator(window.Close));
+            var content = new VMHostWindowViewModel(contentGenerator(window.Close), backgroundColor);
             SetupWindow(window, content, resizeable, height, width);
             window.Show();
         }
 
-        public static bool? CreateAndShowModal(Func<Action, ViewModelBase> contentGenerator, bool resizeable, double height, double width) {
+        public static bool? CreateAndShowModal(Func<Action, ViewModelBase> contentGenerator, bool resizeable, double height, double width, string backgroundColor) {
             var window = new VMHostWindowView();
-            var content = new VMHostWindowViewModel(contentGenerator(window.Close));
+            var content = new VMHostWindowViewModel(contentGenerator(window.Close), backgroundColor);
             SetupWindow(window, content, resizeable, height, width);
             return window.ShowDialog();
         }
 
-        public static void Popup(string message, double height, double width) {
-            CreateAndShowModal(x => new MessageViewModel(message), false, height, width);
+        public static void Popup(string message, double height, double width, string backgroundColor) {
+            CreateAndShowModal(x => new MessageViewModel(message), false, height, width, backgroundColor);
         }
 
         private static void SetupWindow(VMHostWindowView window, VMHostWindowViewModel content, bool resizeable, double height, double width) {
